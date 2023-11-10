ea5e882904f8f686c48bcd3ec5f718782f0f376e
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2023-11-10 23:00:12 +0100

    commit

--- components/TransactionCard/TransactionCard.tsx
+++ components/TransactionCard/TransactionCard.tsx
@@ -1,33 +1,33 @@
-import React from "react";
-import styles from "../../src/styles/TransactionCard.module.css";
-import { TransactionProps } from "../../Types/types";
+// import React from "react";
+// import styles from "../../src/styles/TransactionCard.module.css";
+// import { TransactionProps } from "../../Types/types";
 
-export default function TransactionCard({ props }: TransactionProps) {
-  console.log(props, "les props");
-  return (
-    <div className={styles["transaction-card"]}>
-      <h2>Transaction:</h2>
-      <p>
-        <span>Date:</span> {props.date}
-      </p>
-      <p>
-        <span>From:</span> {props.from}
-      </p>
-      <p>
-        <span>Category:</span> {props.category}
-      </p>
-      <p>
-        <span>Amount:</span> {props.payment}
-      </p>
-      <p>
-        <span>Participants:</span>
-      </p>
-      <ul>
-        <br />
-        {props.participants.map((participant, index) => (
-          <li key={index}>{participant}/ </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+// export default function TransactionCard({ props }: TransactionProps) {
+//   console.log(props, "les props");
+//   return (
+//     <div className={styles["transaction-card"]}>
+//       <h2>Transaction:</h2>
+//       <p>
+//         <span>Date:</span> {props.date}
+//       </p>
+//       <p>
+//         <span>From:</span> {props.from}
+//       </p>
+//       <p>
+//         <span>Category:</span> {props.category}
+//       </p>
+//       <p>
+//         <span>Amount:</span> {props.payment}
+//       </p>
+//       <p>
+//         <span>Participants:</span>
+//       </p>
+//       <ul>
+//         <br />
+//         {props.participants.map((participant, index) => (
+//           <li key={index}>{participant}/ </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
