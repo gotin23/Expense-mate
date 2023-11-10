f1c08454f01dcc9cc15a779b81463e153d10d990
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2023-11-10 22:58:42 +0100

    suppression de code

--- components/MyGroup/MyGroup.tsx
+++ components/MyGroup/MyGroup.tsx
@@ -1,55 +1,55 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../../Types/types";
-import UserCard from "../../components/UserCard/UserCard";
-import styles from "../../src/styles/MyGroup.module.css";
-import AllTransactions from "../../components/AllTransactions/AllTransactions";
-import AddUserToGroup from "../AddUserToGroup/AddUserToGroup";
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import { useSelector } from "react-redux";
+// import { RootState } from "../../Types/types";
+// import UserCard from "../../components/UserCard/UserCard";
+// import styles from "../../src/styles/MyGroup.module.css";
+// // import AllTransactions from "../../components/AllTransactions/AllTransactions";
+// import AddUserToGroup from "../AddUserToGroup/AddUserToGroup";
 
-export default function MyGroup() {
-  const users = useSelector((state: RootState) => state.user);
-  const [toggleAllTransactions, setToggleAllTransactions] = useState(false);
-  const [toggleAddUser, setToggleAddUser] = useState(false);
+// export default function MyGroup() {
+//   const users = useSelector((state: RootState) => state.user);
+//   const [toggleAllTransactions, setToggleAllTransactions] = useState(false);
+//   const [toggleAddUser, setToggleAddUser] = useState(false);
 
-  const handleToggleAllTransactions = () => {
-    setToggleAllTransactions(!toggleAllTransactions);
-  };
-  const handleToggleAddUser = () => {
-    setToggleAddUser(!toggleAddUser);
-  };
+//   const handleToggleAllTransactions = () => {
+//     setToggleAllTransactions(!toggleAllTransactions);
+//   };
+//   const handleToggleAddUser = () => {
+//     setToggleAddUser(!toggleAddUser);
+//   };
 
-  return (
-    <div>
-      <div className={styles.container}>
-        {/* <h2 className={styles["group-title"]}>My group!</h2> */}
-        <div className={styles["btns-container"]}>
-          {" "}
-          <button onClick={handleToggleAddUser} className={styles["btn-add-user"]}>
-            Add user +
-          </button>
-          <button type="button" className={styles["btn-all-transactions"]} onClick={handleToggleAllTransactions}>
-            {!toggleAllTransactions ? "All transactions" : "Back"}
-          </button>
-        </div>
+//   return (
+//     <div>
+//       <div className={styles.container}>
+//         {/* <h2 className={styles["group-title"]}>My group!</h2> */}
+//         <div className={styles["btns-container"]}>
+//           {" "}
+//           <button onClick={handleToggleAddUser} className={styles["btn-add-user"]}>
+//             Add user +
+//           </button>
+//           <button type="button" className={styles["btn-all-transactions"]} onClick={handleToggleAllTransactions}>
+//             {!toggleAllTransactions ? "All transactions" : "Back"}
+//           </button>
+//         </div>
 
-        {toggleAddUser && <AddUserToGroup setToggle={setToggleAddUser} />}
-        {users.users.length > 0 ? (
-          <>
-            {!toggleAllTransactions ? (
-              <div className={styles["my-group-container"]}>
-                {users.users.map((user) => (
-                  <UserCard key={user.id} props={user} />
-                ))}
-              </div>
-            ) : (
-              <AllTransactions />
-            )}
-          </>
-        ) : (
-          <p className={styles.empty}>Nobody inside the group</p>
-        )}
-      </div>
-    </div>
-  );
-}
+//         {toggleAddUser && <AddUserToGroup setToggle={setToggleAddUser} />}
+//         {users.users.length > 0 ? (
+//           <>
+//             {!toggleAllTransactions ? (
+//               <div className={styles["my-group-container"]}>
+//                 {users.users.map((user) => (
+//                   <UserCard key={user.id} props={user} />
+//                 ))}
+//               </div>
+//             ) : (
+//               <AllTransactions />
+//             )}
+//           </>
+//         ) : (
+//           <p className={styles.empty}>Nobody inside the group</p>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
