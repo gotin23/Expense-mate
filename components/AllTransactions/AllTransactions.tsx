8c5757a9f46cb9d5893261ca75ee557e3ca3a631
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2023-11-10 22:53:06 +0100

    commit

--- components/AllTransactions/AllTransactions.tsx
+++ components/AllTransactions/AllTransactions.tsx
@@ -1,92 +1,92 @@
-import React from "react";
-import { useState } from "react";
-import styles from "../../src/styles/AllTransaction.module.css";
-import TransactionCard from "../TransactionCard/TransactionCard";
-import { useSelector } from "react-redux";
-import { RootState } from "../../Types/types";
+// import React from "react";
+// import { useState } from "react";
+// import styles from "../../src/styles/AllTransaction.module.css";
+// import TransactionCard from "../TransactionCard/TransactionCard";
+// import { useSelector } from "react-redux";
+// import { RootState } from "../../Types/types";
 
-export default function AllTransactions() {
-  const users = useSelector((state: RootState) => state.user);
-  const names: string[] = users.users.map((user) => user.name);
-  names.unshift("all-Users");
-  const [selectedOption, setSelectedOption] = useState("all-Users");
-  const [currentPage, setCurrentPage] = useState(1);
-  const transactionsPerPage = 6;
+// export default function AllTransactions() {
+//   const users = useSelector((state: RootState) => state.user);
+//   const names: string[] = users.users.map((user) => user.name);
+//   names.unshift("all-Users");
+//   const [selectedOption, setSelectedOption] = useState("all-Users");
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const transactionsPerPage = 6;
 
-  const indexOfLastTransaction = currentPage * transactionsPerPage;
-  const indexOfFirstTransaction = indexOfLastTransaction - transactionsPerPage;
-  const currentTransactions = users.allTransactions.slice(indexOfFirstTransaction, indexOfLastTransaction);
-  const filteredTransactions = selectedOption === "all-Users" ? users.allTransactions : users.allTransactions.filter((transaction) => transaction.from === selectedOption);
+//   const indexOfLastTransaction = currentPage * transactionsPerPage;
+//   const indexOfFirstTransaction = indexOfLastTransaction - transactionsPerPage;
+//   const currentTransactions = users.allTransactions.slice(indexOfFirstTransaction, indexOfLastTransaction);
+//   const filteredTransactions = selectedOption === "all-Users" ? users.allTransactions : users.allTransactions.filter((transaction) => transaction.from === selectedOption);
 
-  const totalPayment = currentTransactions.reduce((total, transaction) => {
-    return total + transaction.payment;
-  }, 0);
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedOption(event.target.value);
-    setCurrentPage(1);
-  };
-  return (
-    <>
-      <div className={styles["all-transactions-container"]}>
-        <div className={styles.search}>
-          <label>Select a user:</label>
-          <select value={selectedOption} onChange={handleSelectChange}>
-            {names.map((name) => (
-              <option key={name} value={name}>
-                {name}
-              </option>
-            ))}
-          </select>
-        </div>
-        {filteredTransactions.length > 0 ? (
-          filteredTransactions.slice(indexOfFirstTransaction, indexOfLastTransaction).map((transaction, index) => (
-            <TransactionCard
-              key={index}
-              props={{
-                payment: transaction.payment,
-                participants: transaction.participants,
-                date: transaction.date,
-                category: transaction.category,
-                from: transaction.from,
-              }}
-            />
-          ))
-        ) : (
-          <p>No transactions made</p>
-        )}
+//   const totalPayment = currentTransactions.reduce((total, transaction) => {
+//     return total + transaction.payment;
+//   }, 0);
+//   const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+//     setSelectedOption(event.target.value);
+//     setCurrentPage(1);
+//   };
+//   return (
+//     <>
+//       <div className={styles["all-transactions-container"]}>
+//         <div className={styles.search}>
+//           <label>Select a user:</label>
+//           <select value={selectedOption} onChange={handleSelectChange}>
+//             {names.map((name) => (
+//               <option key={name} value={name}>
+//                 {name}
+//               </option>
+//             ))}
+//           </select>
+//         </div>
+//         {filteredTransactions.length > 0 ? (
+//           filteredTransactions.slice(indexOfFirstTransaction, indexOfLastTransaction).map((transaction, index) => (
+//             <TransactionCard
+//               key={index}
+//               props={{
+//                 payment: transaction.payment,
+//                 participants: transaction.participants,
+//                 date: transaction.date,
+//                 category: transaction.category,
+//                 from: transaction.from,
+//               }}
+//             />
+//           ))
+//         ) : (
+//           <p>No transactions made</p>
+//         )}
 
-        <div className={styles.pagination}>
-          {filteredTransactions.length > transactionsPerPage && (
-            <ul>
-              {/* Bouton pour la première page */}
-              <li>
-                <button onClick={() => setCurrentPage(1)}>1</button>
-              </li>
+//         <div className={styles.pagination}>
+//           {filteredTransactions.length > transactionsPerPage && (
+//             <ul>
+//               {/* Bouton pour la première page */}
+//               <li>
+//                 <button onClick={() => setCurrentPage(1)}>1</button>
+//               </li>
 
-              {/* Bouton pour la page précédente (si currentPage n'est pas déjà 1) */}
-              {currentPage > 1 && (
-                <li>
-                  <button onClick={() => setCurrentPage(currentPage - 1)}>&lt;</button>
-                </li>
-              )}
+//               {/* Bouton pour la page précédente (si currentPage n'est pas déjà 1) */}
+//               {currentPage > 1 && (
+//                 <li>
+//                   <button onClick={() => setCurrentPage(currentPage - 1)}>&lt;</button>
+//                 </li>
+//               )}
 
-              {/* Bouton pour la page suivante (si la page suivante existe) */}
-              {currentPage < Math.ceil(filteredTransactions.length / transactionsPerPage) && (
-                <li>
-                  <button onClick={() => setCurrentPage(currentPage + 1)}>&gt;</button>
-                </li>
-              )}
+//               {/* Bouton pour la page suivante (si la page suivante existe) */}
+//               {currentPage < Math.ceil(filteredTransactions.length / transactionsPerPage) && (
+//                 <li>
+//                   <button onClick={() => setCurrentPage(currentPage + 1)}>&gt;</button>
+//                 </li>
+//               )}
 
-              {/* Bouton pour la dernière page */}
-              <li>
-                <button onClick={() => setCurrentPage(Math.ceil(filteredTransactions.length / transactionsPerPage))}>
-                  {Math.ceil(filteredTransactions.length / transactionsPerPage)}
-                </button>
-              </li>
-            </ul>
-          )}
-        </div>
-      </div>
-    </>
-  );
-}
+//               {/* Bouton pour la dernière page */}
+//               <li>
+//                 <button onClick={() => setCurrentPage(Math.ceil(filteredTransactions.length / transactionsPerPage))}>
+//                   {Math.ceil(filteredTransactions.length / transactionsPerPage)}
+//                 </button>
+//               </li>
+//             </ul>
+//           )}
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
--- src/pages/MyGroup.tsx
+++ src/pages/MyGroup.tsx
@@ -1,33 +1,33 @@
-import React from "react";
-import { useState } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../../Types/types";
-import UserCard from "../../components/UserCard/UserCard";
-import styles from "../styles/MyGroup.module.css";
-import AllTransactions from "../../components/AllTransactions/AllTransactions";
+// import React from "react";
+// import { useState } from "react";
+// import { useSelector } from "react-redux";
+// import { RootState } from "../../Types/types";
+// import UserCard from "../../components/UserCard/UserCard";
+// import styles from "../styles/MyGroup.module.css";
+// import AllTransactions from "../../components/AllTransactions/AllTransactions";
 
-export default function MyGroup() {
-  const users = useSelector((state: RootState) => state.user);
-  const [toggleAllTransactions, setToggleAllTransactions] = useState(false);
+// export default function MyGroup() {
+//   const users = useSelector((state: RootState) => state.user);
+//   const [toggleAllTransactions, setToggleAllTransactions] = useState(false);
 
-  const handleToggleAllTransactions = () => {
-    setToggleAllTransactions(!toggleAllTransactions);
-  };
+//   const handleToggleAllTransactions = () => {
+//     setToggleAllTransactions(!toggleAllTransactions);
+//   };
 
-  return (
-    <div className={styles.container}>
-      <button type="button" className={styles["btn-all-transactions"]} onClick={handleToggleAllTransactions}>
-        {toggleAllTransactions ? "All transactions" : "Back"}
-      </button>
-      {toggleAllTransactions ? (
-        <div className={styles["my-group-container"]}>
-          {users.users.map((user) => (
-            <UserCard key={user.id} props={user} />
-          ))}
-        </div>
-      ) : (
-        <AllTransactions />
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div className={styles.container}>
+//       <button type="button" className={styles["btn-all-transactions"]} onClick={handleToggleAllTransactions}>
+//         {toggleAllTransactions ? "All transactions" : "Back"}
+//       </button>
+//       {toggleAllTransactions ? (
+//         <div className={styles["my-group-container"]}>
+//           {users.users.map((user) => (
+//             <UserCard key={user.id} props={user} />
+//           ))}
+//         </div>
+//       ) : (
+//         <AllTransactions />
+//       )}
+//     </div>
+//   );
+// }
